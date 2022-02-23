6481e8d3504b84be9494b476805d1c789ef29e9a
shibafu <shibafu528@gmail.com>
2022-02-24 01:29:28 +0900

    gui_window_rewindstatus に対応

--- mshell/MikutterWindowController.swift
+++ mshell/MikutterWindowController.swift
@@ -3,11 +3,15 @@
 //
 
 import Cocoa
+import GRPC
 
 class MikutterWindowController: NSWindowController {
     @IBOutlet weak var paneStack: NSStackView!
-    
+    @IBOutlet weak var statusMessage: NSTextField!
+
     let remote: RemoteWidget
+    var statusMessageStack: [(String, Date?)] = []
+    var rewindStatusSubscription: ServerStreamingCall<Mrpc_SubscribeRequest, Mrpc_Event>?
     
     required init(remote: RemoteWidget) {
         self.remote = remote
@@ -20,6 +24,37 @@ class MikutterWindowController: NSWindowController {
     
     override var windowNibName: NSNib.Name? {
         return String(describing: type(of: self))
+    }
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        statusMessageStack.append((statusMessage.stringValue, nil))
+        rewindStatusSubscription = App.mrpc.subscribe(.with { $0.name = "gui_window_rewindstatus" }, callOptions: nil) { event in
+            guard event.param.count == 3 else { return }
+            let remoteWindow = event.param[0].proxy
+            let text = event.param[1].sval
+            let expire = event.param[2].ival
+            
+            guard remoteWindow == self.remote.proxy() else { return }
+            if expire == 0 {
+                self.statusMessageStack.append((text, nil))
+            } else {
+                let expireDate = Date().addingTimeInterval(TimeInterval(expire))
+                self.statusMessageStack.append((text, expireDate))
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(Int(expire))) {
+                    self.statusMessageStack = self.statusMessageStack.filter { (_, exp) in
+                        exp == nil || exp! > Date()
+                    }
+                    self.statusMessage.stringValue = self.statusMessageStack.last!.0
+                }
+            }
+            DispatchQueue.main.async {
+                self.statusMessage.stringValue = self.statusMessageStack.last!.0
+            }
+        }
+        rewindStatusSubscription!.status.whenComplete { result in
+            print("finished subscription gui_window_rewindstatus: \(result)")
+        }
     }
     
     func join(remotePane: RemoteWidget) {
