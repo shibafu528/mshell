a4f8c92f38c4c68a519ab75fe6603883a496cd38
shibafu <shibafu528@gmail.com>
2022-02-24 00:35:53 +0900

    add applicationShouldTerminateAfterLastWindowClosed

--- mshell/AppDelegate.swift
+++ mshell/AppDelegate.swift
@@ -116,6 +116,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 }
 
 var App: AppDelegate {
